9d349385a6b76d1797c4c057821162129b06eb3f
daw <962983053@qq.com>
2022-09-08 18:26:44 +0800

    feat 获取代码模板接口

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,26 @@
 const Router = require('koa-router');
+const { resolve } = require('path');
+const fs = require('fs');
 
 const router = new Router({
     prefix: '/base'
 })
 
 router.get('/codeTemplate', ctx => {
+    // 代码模板目录路径
+    const codeTempPath = resolve(__dirname, '../packages/online/src/mobile-maker/codeTemplate')
+    // 需要读取的文件
+    const tempList = ['config.vue', 'render.vue']
+    
+    const res = {};
+    tempList.forEach(fileName => {
+        const codeString = fs.readFileSync(resolve(codeTempPath, fileName), 'utf-8')
+        res[fileName] = codeString
+    })
+
     ctx.body = {
         code: 0,
-        message: '代码模板'
+        data: res
     }
 })
 
